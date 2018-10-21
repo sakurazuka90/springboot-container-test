a110b72ca531512ff8c70546d6cbe1be73c7270a
sakurazuka90 <sakurazuka90@gmail.com>
2018-10-21 23:05:34 +0200

    Wypisywanie ip

--- src/main/java/pl/gameeater/SpringbootTestDocker.java
+++ src/main/java/pl/gameeater/SpringbootTestDocker.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class SpringbootTestDocker {
     @RequestMapping("/")
-    public String home() {
-        return "Nie chce mi się pracować po godzinach ale muszę. I nikt mi za to nie zapłaci.";
+    public String home() throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+
+        return "Wiadomość z kontenera o ip: " + inetAddress.getHostAddress();
     }
 
     public static void main(String[] args) {
